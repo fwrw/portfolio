7fb304d9d2c44077c82f3212dc3918e0fbedcfbe
fwrw <heretostayost@gmail.com>
2024-02-08 04:45:20 -0300

    bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,27 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId)
+  if (section) {
+    const offsetTop = section.offsetTop;
+    window.scrollTo({
+      top: offsetTop
+    });
+  }
+}
 
+document.getElementById('about-btn').addEventListener('click', () =>{
+  scrollToSection('about');
 });
+
+document.getElementById('projects-btn').addEventListener('click', () =>{
+  scrollToSection('projects');
+});
+
+document.getElementById('learning-btn').addEventListener('click', () =>{
+  scrollToSection('learning');
+});
+
+document.getElementById('contact-btn').addEventListener('click', () =>{
+  scrollToSection('contact');
+});
+
+
